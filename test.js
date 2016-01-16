dec302042f5b21d505611a2a2c5c9cac967860ce
ragingwind <ragingwind@gmail.com>
2016-01-16 09:05:19 +0900

    Using ava

--- test.js
+++ test.js
@@ -1,28 +1,29 @@
 'use strict';
-var assert = require('assert');
-var headerize = require('./');
 
-it('should returns expected header', function () {
-	assert.strictEqual(headerize('lowerUpper'), 'Lower Upper');
-	assert.strictEqual(headerize('lowerUpUp'), 'Lower Up Up');
-	assert.strictEqual(headerize('lowerUP'), 'Lower UP');
-	assert.strictEqual(headerize('lowerUPDUser'), 'Lower UPD User');
-	assert.strictEqual(headerize('lowerUPDUs'), 'Lower UPDUs');
-	assert.strictEqual(headerize('lowerlowerUpper'), 'Lowerlower Upper');
-	assert.strictEqual(headerize('lower upper'), 'Lower Upper');
+import test from 'ava';
+import headerize from './';
+
+test('should returns expected header', t => {
+	t.is(headerize('lowerUpper'), 'Lower Upper');
+	t.is(headerize('lowerUpUp'), 'Lower Up Up');
+	t.is(headerize('lowerUP'), 'Lower UP');
+	t.is(headerize('lowerUPDUser'), 'Lower UPD User');
+	t.is(headerize('lowerUPDUs'), 'Lower UPDUs');
+	t.is(headerize('lowerlowerUpper'), 'Lowerlower Upper');
+	t.is(headerize('lower upper'), 'Lower Upper');
 });
 
-it('should returns expected header from pre-none-letter string', function () {
-	assert.strictEqual(headerize(' lowerUpper', {trim: true}), 'Lower Upper');
-	assert.strictEqual(headerize('_lowerUpper', {trim: true}), 'Lower Upper');
-	assert.strictEqual(headerize('.lowerUpper', {trim: true}), 'Lower Upper');
-	assert.strictEqual(headerize('/lowerUpper', {trim: true}), 'Lower Upper');
-	assert.strictEqual(headerize(' /._lowerUpper', {trim: true}), 'Lower Upper');
+test('should returns expected header from pre-none-letter string', t => {
+	t.is(headerize(' lowerUpper', {trim: true}), 'Lower Upper');
+	t.is(headerize('_lowerUpper', {trim: true}), 'Lower Upper');
+	t.is(headerize('.lowerUpper', {trim: true}), 'Lower Upper');
+	t.is(headerize('/lowerUpper', {trim: true}), 'Lower Upper');
+	t.is(headerize(' /._lowerUpper', {trim: true}), 'Lower Upper');
 });
 
-it('should returns expected header from middle-none-letter string', function () {
-	assert.strictEqual(headerize('lower-Upper'), 'Lower Upper');
-	assert.strictEqual(headerize('lower Upper'), 'Lower Upper');
-	assert.strictEqual(headerize('lower.Upper'), 'Lower Upper');
-	assert.strictEqual(headerize('lower.Upper'), 'Lower Upper');
+test('should returns expected header from middle-none-letter string', t => {
+	t.is(headerize('lower-Upper'), 'Lower Upper');
+	t.is(headerize('lower Upper'), 'Lower Upper');
+	t.is(headerize('lower.Upper'), 'Lower Upper');
+	t.is(headerize('lower.Upper'), 'Lower Upper');
 });
